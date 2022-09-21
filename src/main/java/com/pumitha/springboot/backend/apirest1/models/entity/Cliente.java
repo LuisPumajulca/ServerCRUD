24e7db2a67932d2bdb0ea758ea94a5e90f623275
LuisPumajulca <pumajulca@hotmail.com>
2022-09-21 17:20:31 -0500

    customizando mensajes de validacion cap 74

--- src/main/java/com/pumitha/springboot/backend/apirest1/models/entity/Cliente.java
+++ src/main/java/com/pumitha/springboot/backend/apirest1/models/entity/Cliente.java
@@ -24,16 +24,16 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotEmpty
-	@Size(min=4, max=12)
+	@NotEmpty(message = "no puede estar vacio")
+	@Size(min=4, max=12, message = "tiene que tener un tamaño entre 4 y 12")
 	@Column(nullable = false)
 	private String nombre;
 	
-	@NotEmpty
+	@NotEmpty(message = "no puede estar vacio")
 	private String apellido;
 	
-	@NotEmpty
-	@Email
+	@NotEmpty(message = "no puede estar vacio")
+	@Email(message = "debe tener una direccion de correo valida")
 	@Column(nullable = false, unique = true)
 	private String email;
 	
