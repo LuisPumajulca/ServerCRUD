1b5dfecc85fb1057c44854dff2fa0281611de7a9
LuisPumajulca <pumajulca@hotmail.com>
2022-09-21 23:31:03 -0500

    implementacion paginacion cap 81-82

--- src/main/java/com/pumitha/springboot/backend/apirest1/models/dao/IClienteDao.java
+++ src/main/java/com/pumitha/springboot/backend/apirest1/models/dao/IClienteDao.java
@@ -1,9 +1,9 @@
 package com.pumitha.springboot.backend.apirest1.models.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pumitha.springboot.backend.apirest1.models.entity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
+public interface IClienteDao extends JpaRepository<Cliente, Long> {
 
 }
--- src/main/java/com/pumitha/springboot/backend/apirest1/models/services/ClienteServiceImpl.java
+++ src/main/java/com/pumitha/springboot/backend/apirest1/models/services/ClienteServiceImpl.java
@@ -3,6 +3,8 @@ package com.pumitha.springboot.backend.apirest1.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,29 +19,31 @@ public class ClienteServiceImpl implements IClienteService {
 	
 	@Override
 	@Transactional(readOnly = true)
-	public List<Cliente> findAll() {
-		
+	public List<Cliente> findAll() {	
 		return (List<Cliente>) clienteDao.findAll();
+	}	
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Cliente> findAll(Pageable pageable) {
+		return clienteDao.findAll(pageable);
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public Cliente findById(Long id) {
-		// TODO Auto-generated method stub
 		return clienteDao.findById(id).orElse(null);
 	}
 
 	@Override
 	@Transactional
 	public Cliente save(Cliente cliente) {
-		// TODO Auto-generated method stub
 		return clienteDao.save(cliente);
 	}
 
 	@Override
 	@Transactional
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
 		clienteDao.deleteById(id);
 		
 	}
